2e480b09eec9d9442aa1560521de9166f80e3976
mahmoud-abulail <Mahmoud.abulail@coxautoinc.com>
2017-08-07 19:28:57 -0400

    chnage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
+
+
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { NgModule, APP_INITIALIZER} from '@angular/core';
+import { NoopAnimationsModule} from '@angular/platform-browser/animations';
+import { HttpModule } from '@angular/http';
+
+import { InputTextModule, ButtonModule, TabViewModule , SelectButtonModule, InputSwitchModule, SharedModule,
+         DropdownModule , ToggleButtonModule, CalendarModule, InputTextareaModule, DataTableModule, SplitButtonModule,
+         MenuModule, StepsModule, ContextMenuModule, DialogModule, FieldsetModule, PickListModule, PanelModule,
+         PaginatorModule, DataGridModule}  from 'primeng/primeng';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +18,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+      BrowserModule, InputTextModule,FormsModule, ButtonModule, TabViewModule, SelectButtonModule, DropdownModule,
+    NoopAnimationsModule, ToggleButtonModule, CalendarModule, InputSwitchModule, InputTextareaModule, DataTableModule, 
+    SharedModule, HttpModule, MenuModule, StepsModule, ContextMenuModule, DialogModule, FieldsetModule, PickListModule,
+    PanelModule, PaginatorModule, DataGridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
